8043ac5a1ced81a2d7803b35cb634acbcd799925
vovo98er <lengockhanh1602@gmail.com>
2019-03-05 18:58:47 +0700

    dELETE PRODUCT

---
+++ Model/DAO/ProductDAO.cs
@@ -0,0 +1,44 @@
+﻿using Model.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    public class ProductDAO
+    {
+        CPM db = null;
+        public ProductDAO()
+        {
+            db = new CPM();
+        }
+        public long Insert(Product entity)
+        {
+            db.Products.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public IEnumerable<Product> ListAllPaging(int page, int pageSize)
+        {
+            return db.Products.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+
+        public bool Delete(int iD)
+        {
+            try
+            {
+                var product = db.Products.Find(iD);
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
